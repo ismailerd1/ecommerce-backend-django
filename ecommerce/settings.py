2c96ab59bd9718b1643a1b0bb2942ac0a302592b
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-09-10 20:09:04 +0300

    product and cart models

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -84,10 +84,22 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce',
+        'USER' : 'root',
+        'HOST': 'localhost',
+        'PASSWORD' : ''
     }
 }
 
--- products/models.py
+++ products/models.py
@@ -1,4 +1,30 @@
+from unicodedata import category
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+class Categories(models.Model):
+    category_name = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    product_image = models.ImageField()
+    slug = models.SlugField()
+    product_price = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(1)])
+    discounted_price = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(1)])
+    product_description = models.TextField()
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    added_at = models.DateTimeField()
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.BigIntegerField()
+
 
