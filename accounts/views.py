d3efe38b0c7c03fee104040d57f686cd0402f379
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-10-03 09:03:04 +0300

    accessing the currently logged in customer

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
+from logging import raiseExceptions
 from django.shortcuts import render
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import *
 from .serializers import *
 
@@ -8,3 +11,16 @@ from .serializers import *
 class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+    @action(detail=False, methods=['GET', 'PUT'])
+    def me(self, request):
+        (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid()
+            serializer.save(raiseExceptions=True)
+            return Response(serializer.data)
